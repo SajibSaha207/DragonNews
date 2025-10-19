e6a2d72f609513307ee94dbbbae8d7012c483bd3
SajibSaha207 <sajibsaha.207@gmail.com>
2025-10-20 01:13:14 +0600

    Rightaside done

---
+++ src/Components/HomeLayout/RightAside.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SocialLogin from './SocialLogin';
+import FindUs from './FindUs';
+
+const RightAside = () => {
+    return (
+        <div className='space-y-8'>
+         <SocialLogin></SocialLogin>
+         <FindUs></FindUs>
+         <Qzone></Qzone>
+        </div>
+    );
+};
+
+export default RightAside;
